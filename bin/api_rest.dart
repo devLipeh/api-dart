ba0a2d74c0411a636bfb05260913d7a365ac0947
Filipe Santos <classicmagic404@gmail.com>
2022-06-26 10:33:02 -0300

    initial commit

---
+++ bin/api_rest.dart
@@ -0,0 +1,5 @@
+import 'package:api_rest/api_rest.dart' as api_rest;
+
+void main(List<String> arguments) {
+  print('Hello world: ${api_rest.calculate()}!');
+}
---
+++ test/api_rest_test.dart
@@ -0,0 +1,8 @@
+import 'package:api_rest/api_rest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
